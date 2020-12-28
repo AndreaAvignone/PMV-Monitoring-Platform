f3d1213df55a5a4353de8dfd326ce8e8bd7ee579
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2020-12-29 00:00:33 +0100

    Add files via upload

--- profiles catalog/profiles_catalog.py
+++ profiles catalog/profiles_catalog.py
@@ -12,30 +12,82 @@ class ProfilesCatalogREST():
 		self.requestResult=requests.get(self.serviceCatalogAddress+"/services/profiles_catalog").json()
 		self.profilesCatalogIP=self.requestResult[0].get("IP_address")
 		self.profilesCatalogPort=self.requestResult[0].get("port")
-
+		self.service=self.requestResult[0].get("service")
 
 	def GET(self,*uri):
-		if len(uri)!=0:
-			try:
-				resource=uri[0]
-				try:
-					parameter=uri[1]
-					output=self.profilesCatalog.RetrieveProfileParameter(resource,parameter)	
-				except:
-					output=self.profilesCatalog.RetrieveProfileInfo(resource)
-					if output==False:
-						output= self.profilesCatalog.profilesContent[resource]
-
-			except:
+		uriLen=len(uri)
+		if uriLen!=0:
+			profile= self.profilesCatalog.retrieveProfileInfo(uri[0])
+			if profile is not False:
+				if uriLen>1:
+					profileInfo= self.profilesCatalog.retrieveProfileParameter(uri[0],uri[1])
+					if profileInfo is not False:
+						output=profileInfo
+					else:
+						output=profile.get(uri[1])
+				else:
+					output=profile
+			else:
+				output=self.profilesCatalog.profilesContent.get(uri[0])
+			if output==None:
 				raise cherrypy.HTTPError(404,"Information Not found")
 
-			if output==False:
-					raise cherrypy.HTTPError(404,"Parameter Not found")
 		else:
 			output=self.profilesCatalog.profilesContent['description']
 
 		return json.dumps(output) 
 
+	def PUT(self, *uri):
+		body=cherrypy.request.body.read()
+		json_body=json.loads(body.decode('utf-8'))
+		command=str(uri[0])
+		if command=='insertProfile':
+			platform_ID=json_body['platform_ID']
+			platform_name=json_body['platform_name']
+			inactiveTime=json_body['inactiveTime']
+			preferences=json_body['preferences']
+			location=json_body['location'] 
+			newProfile=self.profilesCatalog.insertProfile(platform_ID,platform_name,inactiveTime,preferences,location)
+			if newProfile==True:
+				output="Profile '{}' has been added to Profiles Database".format(platform_ID)
+			else:
+				output="'{}' already exists!".format(platform_ID)
+			print(output)
+		else:
+			raise cherrypy.HTTPError(501, "No operation!")
+		
+
+	def POST(self, *uri):
+		body=cherrypy.request.body.read()
+		json_body=json.loads(body.decode('utf-8'))
+		command=str(uri[0])
+		if command=='setParameter':
+			platform_ID=json_body['platform_ID']
+			parameter=json_body['parameter']
+			parameter_value=json_body['parameter_value']
+			newSetting=self.profilesCatalog.setParameter(platform_ID,parameter,parameter_value)
+			if newSetting==True:
+				output="Platform '{}': {} is now {}".format(platform_ID, parameter,parameter_value)
+			else:
+				output="Platform '{}': Can't change {} ".format(platform_ID, parameter)
+			print(output)
+		else:
+			raise cherrypy.HTTPError(501, "No operation!")
+
+	def DELETE(self,*uri):
+		command=str(uri[0])
+		if command=='removeProfile':
+			platform_ID=uri[1]
+			removedProfile=self.profilesCatalog.removeProfile(platform_ID) 
+			if removedProfile==True:
+				output="Profile '{}' removed".format(platform_ID)
+			else:
+				output="Profile '{}' not found ".format(platform_ID)
+			print(output)
+		else:
+			raise cherrypy.HTTPError(501, "No operation!")
+
+		
 if __name__ == '__main__':
     db=sys.argv[1]
     profilesCatalog=ProfilesCatalogREST(db)
@@ -45,7 +97,7 @@ if __name__ == '__main__':
             'tools.sessions.on': True
         }
     }
-    cherrypy.tree.mount(profilesCatalog, '/Monitoring-Platform/profiles', conf)
+    cherrypy.tree.mount(profilesCatalog, profilesCatalog.service, conf)
     cherrypy.config.update({'server.socket_host': profilesCatalog.profilesCatalogIP})
     cherrypy.config.update({'server.socket_port': profilesCatalog.profilesCatalogPort})
     cherrypy.engine.start()
--- profiles catalog/profiles_class.py
+++ profiles catalog/profiles_class.py
@@ -1,18 +1,33 @@
 import json
+import time
+from datetime import datetime
+
+class NewProfile():
+    def __init__(self,platform_ID,platform_name,inactiveTime,preferences, location, lastUpdate):
+        self.platform_ID=platform_ID
+        self.platform_name=platform_name
+        self.inactiveTime=inactiveTime
+        self.preferences=preferences
+        self.location=location
+        self.lastUpdate=lastUpdate
+        
+    def jsonify(self):
+        profile={'platform_ID':self.platform_ID,'platform_name':self.platform_name,'inactiveTime':self.inactiveTime,'preferences':self.preferences,'location':self.location,'last_update':self.lastUpdate}
+        return profile
 
 class ProfilesCatalog():
 	def __init__(self, db_filename):
 		self.db_filename=db_filename
 		self.profilesContent=json.load(open(self.db_filename,"r")) #store the database as a variable
-		self.ProfilesListCreate()
+		#self.profilesListCreate()
 
-	def ProfilesListCreate(self):
+	def profilesListCreate(self):
 		self.profilesList=[]
 		for profile in self.profilesContent['profiles_list']:
 			self.profilesList.append(profile['platform_ID'])
 		return self.profilesList
 
-	def RetrieveProfileInfo(self,platform_ID):
+	def retrieveProfileInfo(self,platform_ID):
 		notFound=1
 		for profile in self.profilesContent['profiles_list']:
 			if profile['platform_ID']==platform_ID:
@@ -21,13 +36,60 @@ class ProfilesCatalog():
 		if notFound==1:
 			return False
 
-	def RetrieveProfileParameter(self,platform_ID,parameter):
-		profile=self.RetrieveProfileInfo(platform_ID)
+	def retrieveProfileParameter(self,platform_ID,parameter):
+		profile=self.retrieveProfileInfo(platform_ID)
 		try:
 			result= profile[parameter]
 		except:
 			result=False
 		return result
+
+	def insertProfile(self,platform_ID,platform_name,inactiveTime,preferences, location):
+		notExisting=1
+		now=datetime.now()
+		timestamp=now.strftime("%d/%m/%Y %H:%M")
+		profile=self.retrieveProfileInfo(platform_ID)
+		if profile is False:
+			createdProfile=NewProfile(platform_ID,platform_name,inactiveTime,preferences,location,timestamp).jsonify()
+			self.profilesContent['profiles_list'].append(createdProfile)
+			return True
+		else:
+			return False
+
+	def removeProfile(self,platform_ID):
+		notFound=1
+		for i in range(len(self.profilesContent['profiles_list'])): 
+			if self.profilesContent['profiles_list'][i]['platform_ID']==platform_ID:
+				notFound=0
+				self.profilesContent['profiles_list'].pop(i) 
+				return True
+		if notFound==1:
+			return False
+
+	def setParameter(self, platform_ID, parameter, parameter_value):
+		notFound=1
+		if parameter != "platform_ID":
+			for i in range(len(self.profilesContent['profiles_list'])):
+				if self.profilesContent['profiles_list'][i]['platform_ID']==platform_ID:
+					notFound=0
+					pos=i
+					break
+			if notFound == 0:
+				self.profilesContent['profiles_list'][pos][parameter]=parameter_value
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	def save(self,db_filename):
+		with open(db_filename,'w') as file:
+			json.dump(self.profilesContent,file, indent=4)
+
+
+
+
+
 
 
 
