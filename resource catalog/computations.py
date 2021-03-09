447d6c47f9567fc3ee76b8153103fafa47723c5c
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-03-09 16:00:51 +0100

    compute mrt for each new value

---
+++ resource catalog/computations.py
@@ -0,0 +1,20 @@
+
+class MRT_calculator(object):
+	def __init__(self):
+        parameters=["temperature","temperature_g","emissivity","diameter","wind"]
+
+    def MRT_calculation(self,temperature,temp_g, wind,eg,d):
+        MRT=(((temp_g+273.15)**4+(1.335*(10**8)*(wind**0.71))/(eg*(d**0.4))*(temp_g-temperature))**0.25)-273.15
+        return MRT
+
+    def MRT_JSON(self,body):
+        temperature=body['temperature']
+        temp_g=body['temperature_g']
+        wind=body['wind']
+        emissivity=body['emissivity']
+        diameter=body['diameter']
+        return self.MRT_calculation(temperature,temperature_g,wind,emissivity,diameter)
+
+
+
+    
