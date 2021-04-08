5cd136d10d5a8c99eb6f66207daa62402e9d9981
Andrea Avignone <andrea.avignone@gmail.com>
2021-04-08 23:01:07 +0200

    profile update for devices

--- profiles catalog/profiles_catalog.py
+++ profiles catalog/profiles_catalog.py
@@ -5,121 +5,160 @@ import time
 import sys
 from profiles_class import ProfilesCatalog
 class ProfilesCatalogREST():
-	exposed=True
-	def __init__(self,db_filename):
-		self.profilesCatalog=ProfilesCatalog(db_filename)
-		self.serviceCatalogAddress=self.profilesCatalog.profilesContent['service_catalog']
-		self.requestResult=requests.get(self.serviceCatalogAddress+"/profiles_catalog").json()
-		self.profilesCatalogIP=self.requestResult.get("IP_address")
-		self.profilesCatalogPort=self.requestResult.get("port")
-		self.service=self.requestResult.get("service")
+    exposed=True
+    def __init__(self,db_filename):
+        self.profilesCatalog=ProfilesCatalog(db_filename)
+        self.serviceCatalogAddress=self.profilesCatalog.profilesContent['service_catalog']
+        self.requestResult=requests.get(self.serviceCatalogAddress+"/profiles_catalog").json()
+        self.profilesCatalogIP=self.requestResult.get("IP_address")
+        self.profilesCatalogPort=self.requestResult.get("port")
+        self.service=self.requestResult.get("service")
+    def getServer(self):
+        requestResult=requests.get(self.serviceCatalogAddress+"/server_catalog").json()
+        self.serverCatalogIP=requestResult.get("IP_address")
+        self.serverCatalogPort=requestResult.get("port")
+        self.serverService=requestResult.get("service")
+        self.serverURL=self.buildAddress(self.serverCatalogIP,self.serverCatalogPort,self.serverService)
+    def buildAddress(self,IP,port, service):
+        finalAddress='http://'+IP+':'+str(port)+service
+        return finalAddress
+    def serverRequest(self,platform_ID,room_ID,info):
+        self.getServer()
+        result=requests.get(self.serverURL+'/'+platform_ID+'/'+room_ID+'/'+info).json()
+        return result
+    def GET(self,*uri):
+        uriLen=len(uri)
+        if uriLen!=0:
+            profile= self.profilesCatalog.retrieveProfileInfo(uri[0])
+            if profile is not False:
+                if uriLen>1:
+                    if uri[1]=="preferences":
+                        for room in profile["preferences"]:
+                            try:
+                                devices=self.serverRequest(uri[0],room["room_ID"],"devices")
+                                self.profilesCatalog.createDevicesList(uri[0],room["room_ID"],devices)
+                            except:
+                                pass
+                    profileInfo= self.profilesCatalog.retrieveProfileParameter(uri[0],uri[1])
+                    if uriLen==3:
+                        try:
+                            pos=self.profilesCatalog.findRoomPos(profileInfo,uri[2])
+                            profileInfo=profileInfo[pos]
+                        except:
+                            pass
+                    if profileInfo is not False:
+                        output=profileInfo
+                    else:
+                        output=profile.get(uri[1])
+                else:
+                    output=profile
+            else:
+                output=self.profilesCatalog.profilesContent.get(uri[0])
+            if output==None:
+                raise cherrypy.HTTPError(404,"Information Not found")
 
-	def GET(self,*uri):
-		uriLen=len(uri)
-		if uriLen!=0:
-			profile= self.profilesCatalog.retrieveProfileInfo(uri[0])
-			if profile is not False:
-				if uriLen>1:
-					profileInfo= self.profilesCatalog.retrieveProfileParameter(uri[0],uri[1])
-					if profileInfo is not False:
-						output=profileInfo
-					else:
-						output=profile.get(uri[1])
-				else:
-					output=profile
-			else:
-				output=self.profilesCatalog.profilesContent.get(uri[0])
-			if output==None:
-				raise cherrypy.HTTPError(404,"Information Not found")
+        else:
+            output=self.profilesCatalog.profilesContent['description']
 
-		else:
-			output=self.profilesCatalog.profilesContent['description']
+        return json.dumps(output) 
 
-		return json.dumps(output) 
-
-	def PUT(self, *uri):
-		body=cherrypy.request.body.read()
-		json_body=json.loads(body.decode('utf-8'))
-		command=str(uri[0])
-		ack=False
-		saveFlag=False
-		if command=='insertProfile':
-			platform_ID=json_body['platform_ID']
-			platform_name=json_body['platform_name']
-			inactiveTime=json_body['inactiveTime']
-			preferences=json_body['preferences']
-			location=json_body['location'] 
-			newProfile=self.profilesCatalog.insertProfile(platform_ID,platform_name,inactiveTime,preferences,location)
-			if newProfile==True:
-				output="Profile '{}' has been added to Profiles Database".format(platform_ID)
-				saveFlag=True
-				ack=True
-			else:
-				output="'{}' already exists!".format(platform_ID)
-		#from app
-		elif command=='insertRoom':
-			platform_ID=uri[1]
-			room_ID=json_body['room_ID']
-			room_name=json_body['room_name']
-			newRoomFlag,newRoom=self.profilesCatalog.insertRoom(platform_ID,room_ID,json_body)
-			if newRoomFlag==True:
-				output="Room '{}' has been added to platform '{}'".format(room_name,platform_ID)
-				saveFlag=True
-				ack=newRoom
-			else:
-				output="Room '{}' cannot be added to platform '{}'".format(room_name,platform_ID)
-		elif command=='associateRoom':
-			platform_ID=uri[1]
-			associatedRoomFlag,associatedRoom=self.profilesCatalog.associateRoom(platform_ID,json_body['timestamp'])
-			if associatedRoomFlag==True:
-				output="Room '{}' has been assoicated in platform '{}'".format(associatedRoom['room_name'],platform_ID)
-				ack=associatedRoom
-				saveFlag=True
-			else:
-				output="Association failed in platform '{}'".format(platform_ID)
+    def PUT(self, *uri):
+        body=cherrypy.request.body.read()
+        json_body=json.loads(body.decode('utf-8'))
+        command=str(uri[0])
+        ack=False
+        saveFlag=False
+        if command=='insertProfile':
+            platform_ID=json_body['platform_ID']
+            platform_name=json_body['platform_name']
+            inactiveTime=json_body['inactiveTime']
+            preferences=json_body['preferences']
+            location=json_body['location'] 
+            newProfile=self.profilesCatalog.insertProfile(platform_ID,platform_name,inactiveTime,preferences,location)
+            if newProfile==True:
+                output="Profile '{}' has been added to Profiles Database".format(platform_ID)
+                saveFlag=True
+                ack=True
+            else:
+                output="'{}' already exists!".format(platform_ID)
+        #from app
+        elif command=='insertRoom':
+            platform_ID=uri[1]
+            room_ID=json_body['room_ID']
+            room_name=json_body['room_name']
+            newRoomFlag,newRoom=self.profilesCatalog.insertRoom(platform_ID,room_ID,json_body)
+            if newRoomFlag==True:
+                output="Room '{}' has been added to platform '{}'".format(room_name,platform_ID)
+                saveFlag=True
+                ack=newRoom
+            else:
+                output="Room '{}' cannot be added to platform '{}'".format(room_name,platform_ID)
+        elif command=='associateRoom':
+            platform_ID=uri[1]
+            associatedRoomFlag,associatedRoom=self.profilesCatalog.associateRoom(platform_ID,json_body['timestamp'])
+            if associatedRoomFlag==True:
+                output="Room '{}' has been assoicated in platform '{}'".format(associatedRoom['room_name'],platform_ID)
+                ack=associatedRoom
+                saveFlag=True
+            else:
+                output="Association failed in platform '{}'".format(platform_ID)
 
 
-		else:
-			raise cherrypy.HTTPError(501, "No operation!")
-		if saveFlag==True:
-			self.profilesCatalog.save()
-		print(output)
-		return json.dumps(ack)
+        else:
+            raise cherrypy.HTTPError(501, "No operation!")
+        if saveFlag==True:
+            self.profilesCatalog.save()
+        print(output)
+        return json.dumps(ack)
 		
 
-	def POST(self, *uri):
-		body=cherrypy.request.body.read()
-		json_body=json.loads(body.decode('utf-8'))
-		command=str(uri[0])
-		if command=='setParameter':
-			platform_ID=uri[1]
-			parameter=json_body['parameter']
-			parameter_value=json_body['parameter_value']
-			newSetting=self.profilesCatalog.setParameter(platform_ID,parameter,parameter_value)
-			if newSetting==True:
-				output="Platform '{}': {} is now {}".format(platform_ID, parameter,parameter_value)
-				self.profilesCatalog.save()
-			else:
-				output="Platform '{}': Can't change {} ".format(platform_ID, parameter)
-			print(output)
-		else:
-			raise cherrypy.HTTPError(501, "No operation!")
+    def POST(self, *uri):
+        body=cherrypy.request.body.read()
+        json_body=json.loads(body.decode('utf-8'))
+        command=str(uri[0])
+        if command=='setParameter':
+            platform_ID=uri[1]
+            parameter=json_body['parameter']
+            parameter_value=json_body['parameter_value']
+            newSetting=self.profilesCatalog.setParameter(platform_ID,parameter,parameter_value)
+            if newSetting==True:
+                output="Platform '{}': {} is now {}".format(platform_ID, parameter,parameter_value)
+                self.profilesCatalog.save()
+            else:
+                output="Platform '{}': Can't change {} ".format(platform_ID, parameter)
+            print(output)
+        elif command=='setRoomParameter':
+            platform_ID=uri[1]
+            room_ID=uri[2]
+            parameter=json_body['parameter']
+            parameter_value=json_body['parameter_value']
+            newSetting=self.profilesCatalog.setRoomParameter(platform_ID,room_ID,parameter,parameter_value)
+            if newSetting==True:
+                output="Platform '{}' - Room '{}': {} is now {}".format(platform_ID,room_ID, parameter,parameter_value)
+                self.profilesCatalog.save()
+            else:
+                output="Platform '{}' Room '{}': Can't change {} ".format(platform_ID, room_ID,parameter)
+            print(output)
 
-	def DELETE(self,*uri):
-		command=str(uri[0])
-		if command=='removeProfile':
-			platform_ID=uri[1]
-			removedProfile=self.profilesCatalog.removeProfile(platform_ID) 
-			if removedProfile==True:
-				output="Profile '{}' removed".format(platform_ID)
-				self.profilesCatalog.save()
-			else:
-				output="Profile '{}' not found ".format(platform_ID)
-			print(output)
-		else:
-			raise cherrypy.HTTPError(501, "No operation!")
+            
+        else:
+            raise cherrypy.HTTPError(501, "No operation!")
 
-		
+    def DELETE(self,*uri):
+        command=str(uri[0])
+        if command=='removeProfile':
+            platform_ID=uri[1]
+            removedProfile=self.profilesCatalog.removeProfile(platform_ID) 
+            if removedProfile==True:
+                output="Profile '{}' removed".format(platform_ID)
+                self.profilesCatalog.save()
+            else:
+                output="Profile '{}' not found ".format(platform_ID)
+            print(output)
+        else:
+            raise cherrypy.HTTPError(501, "No operation!")
+
+        
 if __name__ == '__main__':
     db=sys.argv[1]
     profilesCatalog=ProfilesCatalogREST(db)
--- profiles catalog/profiles_class.py
+++ profiles catalog/profiles_class.py
@@ -17,114 +17,164 @@ class NewProfile():
         return profile
 
 class ProfilesCatalog():
-	def __init__(self, db_filename):
-		self.db_filename=db_filename
-		self.profilesContent=json.load(open(self.db_filename,"r")) #store the database as a variable
-		#self.profilesListCreate()
+    def __init__(self, db_filename):
+        self.db_filename=db_filename
+        self.profilesContent=json.load(open(self.db_filename,"r")) #store the database as a variable
+        #self.profilesListCreate()
 
-	def profilesListCreate(self):
-		self.profilesList=[]
-		for profile in self.profilesContent['profiles']:
-			self.profilesList.append(profile['platform_ID'])
-		return self.profilesList
+    def profilesListCreate(self):
+        self.profilesList=[]
+        for profile in self.profilesContent['profiles']:
+            self.profilesList.append(profile['platform_ID'])
+        return self.profilesList
 
-	def findPos(self,platform_ID):
-		notFound=1
-		for i in range(len(self.profilesContent['profiles'])): 
-			if self.profilesContent['profiles'][i]['platform_ID']==platform_ID:
-				notFound=0
-				return i
-		if notFound==1:
-			return False
+    def findPos(self,platform_ID):
+        notFound=1
+        for i in range(len(self.profilesContent['profiles'])): 
+            if self.profilesContent['profiles'][i]['platform_ID']==platform_ID:
+                notFound=0
+                return i
+        if notFound==1:
+            return False
+    def findRoomPos(self,rooms,room_ID):
+        notFound=1
+        for i in range(len(rooms)): 
+            if rooms[i]['room_ID']==room_ID:
+                notFound=0
+                return i
+        if notFound==1:
+            return False
 
-	def retrieveProfileInfo(self,platform_ID):
-		notFound=1
-		for profile in self.profilesContent['profiles']:
-			if profile['platform_ID']==platform_ID:
-				notFound=0
-				return profile
-		if notFound==1:
-			return False
+    def retrieveProfileInfo(self,platform_ID):
+        notFound=1
+        for profile in self.profilesContent['profiles']:
+            if profile['platform_ID']==platform_ID:
+                notFound=0
+                return profile
+        if notFound==1:
+            return False
 
-	def retrieveProfileParameter(self,platform_ID,parameter):
-		profile=self.retrieveProfileInfo(platform_ID)
-		try:
-			result= profile[parameter]
-		except:
-			result=False
-		return result
+    def retrieveProfileParameter(self,platform_ID,parameter):
+        profile=self.retrieveProfileInfo(platform_ID)
+        try:
+            result= profile[parameter]
+        except:
+            result=False
+        return result
 
-	def insertProfile(self,platform_ID,platform_name,inactiveTime,preferences, location):
-		notExisting=1
-		now=datetime.now()
-		timestamp=now.strftime("%d/%m/%Y %H:%M")
-		profile=self.retrieveProfileInfo(platform_ID)
-		if profile is False:
-			createdProfile=NewProfile(platform_ID,platform_name,inactiveTime,preferences,location,timestamp).jsonify()
-			self.profilesContent['profiles'].append(createdProfile)
-			self.profilesContent['profiles_list'].append(platform_ID)
-			return True
-		else:
-			return False
+    def insertProfile(self,platform_ID,platform_name,inactiveTime,preferences, location):
+        notExisting=1
+        now=datetime.now()
+        timestamp=now.strftime("%d/%m/%Y %H:%M")
+        profile=self.retrieveProfileInfo(platform_ID)
+        if profile is False:
+            createdProfile=NewProfile(platform_ID,platform_name,inactiveTime,preferences,location,timestamp).jsonify()
+            self.profilesContent['profiles'].append(createdProfile)
+            self.profilesContent['profiles_list'].append(platform_ID)
+            return True
+        else:
+            return False
 
-	def insertRoom(self,platform_ID,room_ID,room_info):
-		pos=self.findPos(platform_ID)
-		roomNotFound=1
-		if pos is not False:
-			room_cnt=self.retrieveProfileParameter(platform_ID,'room_cnt')+1
-			room_info['room_ID']=room_ID+str(room_cnt)
-			room_info['connection_flag']=False
-			room_info['devices']=[]
-			timestamp=time.time()
-			room_info['connection_timestamp']=timestamp
-			for room in self.profilesContent['profiles'][pos]['preferences']:
-				if room['room_name']==room_info['room_name']:
-					roomNotFound=0
-					break
-			if roomNotFound==1:
-				self.profilesContent['profiles'][pos]['preferences'].append(room_info)
-				self.setParameter(platform_ID,'room_cnt',room_cnt)
-				return True,room_info
-			else:
-				return False,False
-		else:
-			return False,False
+    def insertRoom(self,platform_ID,room_ID,room_info):
+        pos=self.findPos(platform_ID)
+        roomNotFound=1
+        if pos is not False:
+            room_cnt=self.retrieveProfileParameter(platform_ID,'room_cnt')+1
+            room_info['room_ID']=room_ID+str(room_cnt)
+            room_info['connection_flag']=False
+            room_info['devices']=[]
+            timestamp=time.time()
+            room_info['connection_timestamp']=timestamp
+            for room in self.profilesContent['profiles'][pos]['preferences']:
+                if room['room_name']==room_info['room_name']:
+                    roomNotFound=0
+                    break
+            if roomNotFound==1:
+                self.profilesContent['profiles'][pos]['preferences'].append(room_info)
+                self.setParameter(platform_ID,'room_cnt',room_cnt)
+                return True,room_info
+            else:
+                return False,False
+        else:
+            return False,False
 
-	def associateRoom(self,platform_ID,request_timestamp):
-		pos=self.findPos(platform_ID)
-		notFound=1
-		if pos is not False:
-			for pref in self.profilesContent['profiles'][pos]['preferences']:
-				if pref['connection_flag'] is False and (request_timestamp-pref['connection_timestamp'])<60:
-					pref['connection_flag']=True
-					notFound=0
-					return True,pref
-			if notFound==1:
-				return False,False
-		else:
-			return False,False
+    def associateRoom(self,platform_ID,request_timestamp):
+        pos=self.findPos(platform_ID)
+        notFound=1
+        if pos is not False:
+            for pref in self.profilesContent['profiles'][pos]['preferences']:
+                if pref['connection_flag'] is False and (request_timestamp-pref['connection_timestamp'])<60:
+                    pref['connection_flag']=True
+                    notFound=0
+                    return True,pref
+            if notFound==1:
+                return False,False
+        else:
+            return False,False
 
 
-	def removeProfile(self,platform_ID):
-		pos=self.findPos(platform_ID)
-		if pos is not False:
-			self.profilesContent['profiles_list'].remove(platform_ID)
-			self.profilesContent['profiles'].pop(pos) 
-			return True
-		else:
-			return False
+    def removeProfile(self,platform_ID):
+        pos=self.findPos(platform_ID)
+        if pos is not False:
+            self.profilesContent['profiles_list'].remove(platform_ID)
+            self.profilesContent['profiles'].pop(pos) 
+            return True
+        else:
+            return False
 
-	def setParameter(self, platform_ID, parameter, parameter_value):
-		pos=self.findPos(platform_ID)
-		if pos is not False:
-			self.profilesContent['profiles'][pos][parameter]=parameter_value
-			return True
-		else:
-			return False
-
-	def save(self):
-		with open(self.db_filename,'w') as file:
-			json.dump(self.profilesContent,file, indent=4)
+    def setParameter(self, platform_ID, parameter, parameter_value):
+        pos=self.findPos(platform_ID)
+        if pos is not False:
+            self.profilesContent['profiles'][pos][parameter]=parameter_value
+            return True
+        else:
+            return False
+        
+    def retrieveRoomInfo(self,rooms,room_ID):
+        notFound=1
+        for room in rooms:
+            if room['room_ID']==room_ID:
+                notFound=0
+                return room
+        if notFound==1:
+            return False
+    def setRoomParameter(self,platform_ID,room_ID,parameter,parameter_value):
+        pos=self.findPos(platform_ID)
+        if pos is not False:
+            rooms=self.profilesContent['profiles'][pos]["preferences"]
+            room=self.retrieveRoomInfo(rooms,room_ID)
+            if room is not False:
+                room[parameter]=parameter_value
+                return True
+            else:
+                return False
+    def createDevicesList(self,platform_ID,room_ID,devices_list):
+        pos=self.findPos(platform_ID)
+        if pos is not False:
+            rooms=self.profilesContent['profiles'][pos]["preferences"]
+            room=self.retrieveRoomInfo(rooms,room_ID)
+            room["devices"]=[]
+            if room is not False:
+                for device in devices_list:
+                    device_new={}
+                    device_new["device_ID"]=device["device_ID"]
+                    device_new["last_update"]=(device["timestamp"])
+                    device_new["parameters"]=[]
+                    for parameter in device['parameters']:
+                        parameter_new={}
+                        parameter_new["parameter"]=(parameter['parameter'])
+                        parameter_new["unit"]=(parameter['unit'])
+                        device_new["parameters"].append(parameter_new)
+                    room["devices"].append(device_new)
+                
+                return True
+            else:
+                return False
+        
+        
+    def save(self):
+        with open(self.db_filename,'w') as file:
+            json.dump(self.profilesContent,file, indent=4)
 
 
 
