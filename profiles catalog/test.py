4cd1ccf5306480fef855af6d632ed0e12fb8ca0a
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-04-26 23:18:28 +0200

    not useful for final develop

--- profiles catalog/test.py
+++
@@ -1,9 +0,0 @@
-
-class Bank(object):
-    def __init__(self):
-        self._money = 100000
- 
-    @property
-    def money(self):
-        """Get the money available."""
-        return self._money
