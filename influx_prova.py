00922c9e8f6a613e6f3956c3c4c743200a4b57fa
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-11-16 08:49:55 +0100

    Delete influx_prova.py

--- influx_prova.py
+++
@@ -1,7 +0,0 @@
-from influxdb import InfluxDBClient
-
-if __name__=='__main__':
-    client=InfluxDBClient(host='localhost',port=8086)
-    client.create_database('esempio')
-    s=client.get_list_database()
-    print(s)
